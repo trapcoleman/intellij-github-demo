fc7b1dd25427ad262cb7ddbeacb16d41cb5fb458
Travis <trapcoleman@gmail.com>
2022-01-11 19:25:41 -0500

    Initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Travis");
+
+//        try {
+//            int count = 0;
+//            while (true) {
+//                Thread.sleep(2 * 1000);
+//                System.out.println("I'm still here! Iteration " + count++);
+//            }
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+    }
+}
